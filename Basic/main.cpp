2d808c80862f52b6b5112d622cfd63cf2ca5acc1
MasterChief-Ayan <118625280+MasterChief-Ayan@users.noreply.github.com>
2025-06-19 06:07:50 +0530

    Add files via upload

---
+++ Basic/main.cpp
@@ -0,0 +1,21 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+#define ll long long int
+#define LD long double
+
+const int N = 100010;
+
+int inf = 1e9;
+int mod = 1e9 + 7;
+
+signed main()
+{
+    //freopen("IN.txt", "r", stdin);
+    //freopen("OUT.txt", "w", stdout);
+    
+    ios_base::sync_with_stdio(0);
+    cin.tie(0); cout.tie(0);
+
+    return 0;
+}
